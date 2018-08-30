8b0c6ce2f60c6554e1e3b46d25801d79ac25d00f
Danielsd12 <42492229+Danielsd12@users.noreply.github.com>
2018-08-30 12:05:29 +0300

    Add files via upload

---
+++ reverser.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 20 11:08:51 2018
+
+@author: SDTechnologies
+"""
+#Switch reverse (Qn3).
+
+s=input("please enter numbers:")
+s = s[::-1]
+print (s)
+
+x=input("please enter words:")
+x = x.upper()
+print(x)
+print(s)
